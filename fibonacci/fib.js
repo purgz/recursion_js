0de1e4d3e99b51885caed32206be9b0c20201ede
Henry Brooks <henbrooks6@gmail.com>
2023-06-07 13:59:34 +0100

    added recursive function for fibonacci

--- fibonacci/fib.js
+++ fibonacci/fib.js
@@ -1,4 +1,5 @@
 //fibonnaci using interative approach
+/*
 let nums = [];
 function fibs(count) {
   nums[0] = 0;
@@ -8,10 +9,35 @@ function fibs(count) {
   nums[1] = 1;
 
   for (let i = 2; i < count; i++) {
-    console.log("ii");
     nums[i] = nums[i - 2] + nums[i - 1];
   }
   return nums;
 }
+*/
 
-console.log(fibs(8));
+//fibonacci using recursion witha  helper function 
+
+let nums = [];
+
+function fibs(count) {
+  nums[0] = 0;
+  if (count == 1) {
+    return nums;
+  }
+  nums[1] = 1;
+
+  for (let i = 2; i < count; i++) {
+    nums[i] = recursiveFibs(i);
+  }
+  return nums;
+}
+
+function recursiveFibs(count) {
+  if (count < 2) {
+    return 1;
+  }
+
+  return recursiveFibs(count - 2) + recursiveFibs(count - 1);
+}
+
+console.log(fibs(9));
